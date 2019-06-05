4fbda7b55b037242d474beb059175f69f8c5c835
John Wilfred <wilfredjohnmontilde19@gmail.com>
2019-06-05 12:14:44 +0800

    Update user schema and model

--- models/users.js
+++ models/users.js
@@ -3,6 +3,18 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  middleName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
   email: {
     type: String,
     required: true
@@ -15,7 +27,7 @@ const userSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'Event'
   }]
-})
+}, {timestamps: true})
 
 const userModel = mongoose.model(
   'User',
